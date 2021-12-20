cc93f55cba878ce1b34e1f151ab1389c0ca10ac0
pahtunn <tuancode2402200@gmail.com>
2021-12-20 09:11:31 +0700

    add Log-in, Language

---
+++ assets/js/carousel.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            // nav: true,
+        },
+        600:{
+            items:1,
+            // nav: true,
+        },
+        1000:{
+            items:1,
+            // nav: true,
+        }
+    }
+})
--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', function () {
         if (window.scrollY > 100) {
             document.getElementById('navbar_top').classList.add('fixed-top');
+            document.getElementById('navbar_top').style.boxShadow = " 0 0 5px 1px rgb(0 0 0 / 15%) ";
             // add padding top to show content behind navbar
             navbar_height = document.querySelector('.navbar').offsetHeight;
             document.body.style.paddingTop = navbar_height + 'px';
         } else {
             document.getElementById('navbar_top').classList.remove('fixed-top');
+            document.getElementById('navbar_top').style.boxShadow = "none";
             // remove padding top from body
             document.body.style.paddingTop = '0';
         }
